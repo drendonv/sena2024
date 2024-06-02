fb5bd9aaec86bac586e0017fa045e4915d279ab9
Diego Rendon <didirendon8110@gmail.com>
2024-06-01 21:10:41 -0500

    Añadir nuevas vistas y modificar configuración de tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    <title>sena2024</title>
-</head>
-<body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+@extends('Layouts.app')
+@section('titulo','consultar productos')
+@section('contenido')
+    
+
+
     <h1> Bienvenido {{$nombre}} a la aplicacion SENA2024</h1>
     <h2>hello</h2>
 
@@ -18,5 +12,5 @@
             <li>{{ $producto['nombre'] }} - Precio: ${{ $producto['precio'] }}</li>
         @endforeach
     </ul>
-</body>
-</html>
+
+    @endsection
